a36108d74490514c4e78cff838b82c760557dc92
xzc-123456 <xuezhaochang@163.com>
2021-01-19 17:25:19 +0800

    商品管理的分类查询

---
+++ mingrui-xzc-basics/mingrui-xzc-basics-eureka-server/src/main/java/com/xzc/RunEurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.xzc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName RunEurekaServerApplication
+ * @Description: TODO
+ * @Author xuezhaochang
+ * @Date 2021/1/19
+ * @Version V1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class RunEurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunEurekaServerApplication.class);
+    }
+}
---
+++ mingrui-xzc-service/mingrui-xzc-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,32 @@
+package com.baidu.shop.service.impl;
+
+import com.baidu.shop.base.BaseApiService;
+import com.baidu.shop.base.Result;
+import com.baidu.shop.entity.CategoryEntity;
+import com.baidu.shop.mapper.CategoryMapper;
+import com.baidu.shop.service.CategoryService;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @ClassName CategoryServiceImpl
+ * @Description: TODO
+ * @Author xuezhaochang
+ * @Date 2021/1/19
+ * @Version V1.0
+ **/
+@RestController
+public class CategoryServiceImpl extends BaseApiService implements CategoryService {
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public Result<List<CategoryEntity>> getCategoryById(Integer pid) {
+        CategoryEntity categoryEntity = new CategoryEntity();
+        categoryEntity.setParentId(pid);
+        List<CategoryEntity> list = categoryMapper.select(categoryEntity);
+        return this.setResultSuccess(list);
+    }
+}
